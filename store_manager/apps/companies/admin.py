46ea44cafb7bff6ebaadffa3fd5973d28c940af1
antoniolaprov <aaal@cesar.school>
2024-09-28 11:58:45 -0300

    adicionar admin.py

--- store_manager/apps/companies/admin.py
+++ store_manager/apps/companies/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin #antes de cadastrar tem q ter algumas empresas ja. ai eu vi que essa funcionalidade admin.py simula um banco de dados.
+from .models import Empresa, Filial
 
-# Register your models here.
+admin.site.register(Empresa)
+admin.site.register(Filial)
